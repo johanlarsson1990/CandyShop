2b90a18578488a76da1d109b7586467f895cf1ea
mabl0200 <blank.malin@gmail.com>
2022-05-25 11:22:36 +0200

    fler ändringar

--- Candyshop/Controllers/WeatherController.cs
+++ Candyshop/Controllers/WeatherController.cs
@@ -11,15 +11,26 @@ namespace Candyshop.Controllers
     {
         public IActionResult Index()
         {
+            //GetWeather();
             //var url = "https://opendata-download-metfcst.smhi.se/api/category/pmp3g/version/2/geotype/point/lon/16.158/lat/58.5812/data.json";
-            var weather = new Weather
-            {
-                Today = DateTime.Now,
-                ImageNr = 0,
-                ImageUrl = "",
-                Temperature = 0.0M
-            };
-            return View(weather);
+            //var weather = new Weather
+            //{
+            //    Today = DateTime.Now,
+            //    ImageNr = 0,
+            //    ImageUrl = "",
+            //    Temperature = 0.0M
+            //};
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult GetWeather()
+        {
+            var url = "https://opendata-download-metfcst.smhi.se/api/category/pmp3g/version/2/geotype/point/lon/16.158/lat/58.5812/data.json";
+            Console.WriteLine(url);
+            return null;
         }
     }
+    
+
 }
