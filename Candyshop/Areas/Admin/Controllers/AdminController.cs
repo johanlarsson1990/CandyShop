b21d21bc2808b2a57e43e643a11b74eb1cba04d0
johanlarsson1990 <johan.larsson@edu.edugrade.se>
2022-05-31 13:45:58 +0200

    Added Categories and return screens

--- Candyshop/Areas/Admin/Controllers/AdminController.cs
+++ Candyshop/Areas/Admin/Controllers/AdminController.cs
@@ -6,10 +6,12 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using Candyshop.Models;
+using Microsoft.AspNetCore.Authorization;
 
 namespace Candyshop.Areas.Admin.Controllers
 {
     [Area("Admin")]
+    [Authorize(Roles = "Admin")]
     public class AdminController : Controller
     {
         private readonly AppDbContext _context;
