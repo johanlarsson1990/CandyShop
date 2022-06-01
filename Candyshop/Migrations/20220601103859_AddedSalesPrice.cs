67a3638b74534d3538167ec0deb422e9963eccb1
johanlarsson1990 <johan.larsson@edu.edugrade.se>
2022-06-01 14:34:50 +0200

    adjusted

---
+++ Candyshop/Migrations/20220601103859_AddedSalesPrice.cs
@@ -0,0 +1,167 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Candyshop.Migrations
+{
+    public partial class AddedSalesPrice : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Candies",
+                keyColumn: "CandyId",
+                keyValue: 2,
+                column: "SalesPrice",
+                value: 1.99m);
+
+            migrationBuilder.UpdateData(
+                table: "Candies",
+                keyColumn: "CandyId",
+                keyValue: 3,
+                column: "SalesPrice",
+                value: 1.95m);
+
+            migrationBuilder.UpdateData(
+                table: "Candies",
+                keyColumn: "CandyId",
+                keyValue: 4,
+                column: "SalesPrice",
+                value: 3.45m);
+
+            migrationBuilder.UpdateData(
+                table: "Candies",
+                keyColumn: "CandyId",
+                keyValue: 6,
+                column: "SalesPrice",
+                value: 2.49m);
+
+            migrationBuilder.UpdateData(
+                table: "Candies",
+                keyColumn: "CandyId",
+                keyValue: 8,
+                column: "SalesPrice",
+                value: 3.59m);
+
+            migrationBuilder.UpdateData(
+                table: "Candies",
+                keyColumn: "CandyId",
+                keyValue: 9,
+                column: "SalesPrice",
+                value: 2.59m);
+
+            migrationBuilder.UpdateData(
+                table: "Candies",
+                keyColumn: "CandyId",
+                keyValue: 10,
+                column: "SalesPrice",
+                value: 2.99m);
+
+            migrationBuilder.UpdateData(
+                table: "Candies",
+                keyColumn: "CandyId",
+                keyValue: 11,
+                column: "SalesPrice",
+                value: 2.99m);
+
+            migrationBuilder.UpdateData(
+                table: "Candies",
+                keyColumn: "CandyId",
+                keyValue: 12,
+                column: "SalesPrice",
+                value: 3.49m);
+
+            migrationBuilder.UpdateData(
+                table: "Candies",
+                keyColumn: "CandyId",
+                keyValue: 14,
+                column: "SalesPrice",
+                value: 1.99m);
+
+            migrationBuilder.UpdateData(
+                table: "Candies",
+                keyColumn: "CandyId",
+                keyValue: 16,
+                column: "SalesPrice",
+                value: 10m);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Candies",
+                keyColumn: "CandyId",
+                keyValue: 2,
+                column: "SalesPrice",
+                value: 0m);
+
+            migrationBuilder.UpdateData(
+                table: "Candies",
+                keyColumn: "CandyId",
+                keyValue: 3,
+                column: "SalesPrice",
+                value: 0m);
+
+            migrationBuilder.UpdateData(
+                table: "Candies",
+                keyColumn: "CandyId",
+                keyValue: 4,
+                column: "SalesPrice",
+                value: 0m);
+
+            migrationBuilder.UpdateData(
+                table: "Candies",
+                keyColumn: "CandyId",
+                keyValue: 6,
+                column: "SalesPrice",
+                value: 0m);
+
+            migrationBuilder.UpdateData(
+                table: "Candies",
+                keyColumn: "CandyId",
+                keyValue: 8,
+                column: "SalesPrice",
+                value: 0m);
+
+            migrationBuilder.UpdateData(
+                table: "Candies",
+                keyColumn: "CandyId",
+                keyValue: 9,
+                column: "SalesPrice",
+                value: 0m);
+
+            migrationBuilder.UpdateData(
+                table: "Candies",
+                keyColumn: "CandyId",
+                keyValue: 10,
+                column: "SalesPrice",
+                value: 0m);
+
+            migrationBuilder.UpdateData(
+                table: "Candies",
+                keyColumn: "CandyId",
+                keyValue: 11,
+                column: "SalesPrice",
+                value: 0m);
+
+            migrationBuilder.UpdateData(
+                table: "Candies",
+                keyColumn: "CandyId",
+                keyValue: 12,
+                column: "SalesPrice",
+                value: 0m);
+
+            migrationBuilder.UpdateData(
+                table: "Candies",
+                keyColumn: "CandyId",
+                keyValue: 14,
+                column: "SalesPrice",
+                value: 0m);
+
+            migrationBuilder.UpdateData(
+                table: "Candies",
+                keyColumn: "CandyId",
+                keyValue: 16,
+                column: "SalesPrice",
+                value: 0m);
+        }
+    }
+}
